213c268190f1dbd8a99f9dfbbed57c5fdfa4bd98
James Michael DuPont <JamesMikeDuPont@googlemail.com>
2012-02-14 15:05:47 +0100

    model

--- buildings/models.py
+++ buildings/models.py
@@ -7,3 +7,10 @@ class Building(models.Model):
     OSMURL = models.CharField(max_length=300) # an url, i dont know if they have different types
     Lat = models.CharField(max_length=20)  #lat/lon of then center
     Lon = models.CharField(max_length=20) # lat/lon of the center of the building
+
+
+class BuildingPoint(models.Model):
+    parent      = models.ForeignKey('Building', null=False, blank=False, related_name='points')
+    OSMNODEID = models.DecimalField(decimal_places=0, max_digits=15) # the osm id
+    Lat = models.CharField(max_length=20)  #lat/lon of then center
+    Lon = models.CharField(max_length=20) # lat/lon of the center of the building
