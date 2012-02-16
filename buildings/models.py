2f413a041985459f2458e062c549aea407eca92c
James Michael DuPont <JamesMikeDuPont@googlemail.com>
2012-02-16 16:16:50 +0100

    update

--- buildings/models.py
+++ buildings/models.py
@@ -14,3 +14,13 @@ class BuildingPoint(models.Model):
     OSMNODEID = models.DecimalField(decimal_places=0, max_digits=15) # the osm id
     Lat = models.CharField(max_length=20)  #lat/lon of then center
     Lon = models.CharField(max_length=20) # lat/lon of the center of the building
+
+
+class Logger(models.Model):
+    location      = models.ForeignKey('Building', null=False, blank=False, related_name='loggers')
+    Number = models.CharField(max_length=60)
+    Description = models.CharField(max_length=120)
+    NetworkAddress = models.CharField(max_length=120)
+    OSMNODEID = models.DecimalField(decimal_places=0, max_digits=15) # the osm id
+    Lat = models.CharField(max_length=20)  #lat/lon of then center
+    Lon = models.CharField(max_length=20) # lat/lon of the center of the building
