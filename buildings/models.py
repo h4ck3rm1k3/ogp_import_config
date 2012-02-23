d5f6092b7fe1701091fe6f3b8db8cb9f329a22e7
James Michael DuPont <JamesMikeDuPont@googlemail.com>
2012-02-23 16:31:08 +0100

    updated model

--- buildings/models.py
+++ buildings/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
 class Building(models.Model):
     Number = models.CharField(max_length=60)
     Description = models.CharField(max_length=120)
-    OSMURL = models.CharField(max_length=300) # an url, i dont know if they have different types
+    OSMWAYID = models.DecimalField(decimal_places=0, max_digits=15) # the osm way id
     Lat = models.CharField(max_length=20)  #lat/lon of then center
     Lon = models.CharField(max_length=20) # lat/lon of the center of the building
 
@@ -16,13 +16,31 @@ class BuildingPoint(models.Model):
     Lon = models.CharField(max_length=20) # lat/lon of the center of the building
 
 
-class Logger(models.Model):
-    location      = models.ForeignKey('Building', null=False, blank=False, related_name='loggers')
-    Number = models.CharField(max_length=60)
-    Description = models.CharField(max_length=120)
-    Description2 = models.CharField(max_length=120)
-    NetworkAddress = models.CharField(max_length=120)
-    OSMNODEID = models.DecimalField(decimal_places=0, max_digits=15) # the osm id
-    Lat = models.CharField(max_length=20)  #lat/lon of then center
-    Lon = models.CharField(max_length=20) # lat/lon of the center of the building
+class Facinet(models.Model):
+    ##
+    Building      = models.ForeignKey('Building', null=False, blank=False, related_name='FacinetNodes')
+    location = models.IntegerField(unique=True, db_column='Location') # 
+    name = models.TextField(db_column='Name') # 
+    connectionstring = models.TextField(db_column='ConnectionString') # 
+    tapidevice = models.TextField(db_column='TapiDevice', blank=True) # 
+    synctime = models.CharField(max_length=3, db_column='SyncTime') # 
+    online = models.CharField(max_length=3, db_column='Online') # 
+    onlineall = models.CharField(max_length=3, db_column='OnlineAll') #    
+    ## location for display
+    Lat = models.CharField(max_length=20)  #lat/lon of facinet collector
+    Lon = models.CharField(max_length=20) # lat/lon of facinet collector
 
+
+class Logger(models.Model):
+    Facinet      = models.ForeignKey('Facinet', null=False, blank=False, related_name='Loggers')
+    loggerindex = models.IntegerField(unique=True, db_column='LoggerIndex') # 
+    name = models.TextField(db_column='Name') # 
+    online = models.IntegerField(db_column='Online') # 
+   ## location for display
+    Lat = models.CharField(max_length=20)  #lat/lon of the logger
+    Lon = models.CharField(max_length=20) # lat/lon of the logger
+
+class LoggerMeasurement(models.Model):
+    Logger      = models.ForeignKey('Logger', null=False, blank=False, related_name='Measurement')
+    timestamp   = models.DateTimeField()
+    measurement = models.DecimalField(max_digits=12, decimal_places=4)
